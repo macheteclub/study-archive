69bce6a4ccff1393edc45d27c218e238f1f34457
xcrya <nikkey3660@gmail.com>
2026-03-25 20:09:00 +0900

    feat: [BOJ] 11399

---
+++ PS/BOJ/10001-20000/11001-12000/11399.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    int sum = 0;
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+        sum += v[i];
+        total += sum;
+    }
+
+    cout << total;
+}
