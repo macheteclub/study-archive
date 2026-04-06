de57eb87fd71474b9687aa605309afc60cc7a9fa
xcrya <nikkey3660@gmail.com>
2026-04-06 21:31:36 +0900

    feat: [BOJ] 9086

---
+++ PS/BOJ/01000-10000/09001-10000/9086.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+//#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        string str;
+        cin >> str;
+
+        cout << str[0] << str.back() << "\n";
+        
+    }
+}
