7963873de4447f2a43189698a7c8ce2461bb4c96
xcrya <nikkey3660@gmail.com>
2026-04-27 22:04:48 +0900

    feat: [BOJ] 28066
    나는 진짜로 요세푸스가 밉다

---
+++ PS/BOJ/20001-30000/28001-29000/28066.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, k;
+    cin >> n >> k;
+
+    deque<int> d;
+    for (int i = 1; i <= n; i++) {
+        d.push_back(i);
+    }
+
+    while (d.size() >= k) {
+        int f = d.front();
+        d.pop_front();
+
+        for (int i = 0; i < k - 1; i++) {
+            d.pop_front();
+        }
+
+        d.push_back(f);
+    }
+
+    cout << d.front();
+}
