62e99cde486de11825664f17ea357161cca0fb1f
xcrya <nikkey3660@gmail.com>
2026-05-24 22:14:27 +0900

    feat: [Jungol] 5889

---
+++ PS/Jungol/5889.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n % 2 == 1) {
+        cout << "Either";
+    } else if (n % 4 == 0) {
+        cout << "Even";
+    } else {
+        cout << "Odd";
+    }
+
+}
